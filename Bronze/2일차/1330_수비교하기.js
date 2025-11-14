7162dd1e808f027b045812a1117d330c208162f1
jin-park0115 <jeanpark0115>
2025-11-14 17:01:26 +0900

    1330 두 수 비교하기

---
+++ Bronze/2일차/1330_수비교하기.js
@@ -0,0 +1,15 @@
+// 2일차부터 조건문
+
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim("").split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+if (A > B) {
+  console.log(">");
+} else if (A < B) {
+  console.log("<");
+} else if (A == B) {
+  console.log("==");
+}
