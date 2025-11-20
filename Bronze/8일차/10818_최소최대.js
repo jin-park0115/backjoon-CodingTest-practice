04dcbf3e48ab03fe26f565d8c660e95b4d540711
jin-park0115 <jeanpark0115>
2025-11-20 21:55:26 +0900

    저장이 안됐었다

--- Bronze/8일차/10818_최소최대.js
+++ Bronze/8일차/10818_최소최대.js
@@ -2,11 +2,10 @@ const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
 let N = input[1].split(" ").map(Number); // [20, 10, 35, 30, 7]
-let res = [];
-res.push(Math.min(...N));
-res.push(Math.max(...N));
+const min = Math.min(...N);
+const max = Math.max(...N);
 
-console.log(res[0], res[1]);
+console.log(min, max); // 3 번째 방식, 가독성이 최고
 
 /*
   최소, 최대
@@ -31,4 +30,17 @@ console.log(res[0], res[1]);
   여기서 ...N 스프레드연산자를 사용한 이유는
   Math.min(), max()는 숫자를 인자로 받는 함수이다,
   그래서 배열인 N을 배열을 얕게 복사하면서, 배열 형태를 없애고 각각의 요소로 펼친다.
+
+
+
+  2번째 방법 통과
+  const fs = require("fs");
+  const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+  let N = input[1].split(" ").map(Number); // [20, 10, 35, 30, 7]
+  let res = [];
+  res.push(Math.min(...N));
+  res.push(Math.max(...N));
+
+  console.log(res[0], res[1]);
 */
