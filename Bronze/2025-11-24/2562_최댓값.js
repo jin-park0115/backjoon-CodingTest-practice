5fc279dc4d3ac2253714422d4fe8ab64b925d676
jin-park0115 <jinpark0115@naver.com>
2025-11-24 21:38:00 +0900

    문제 풀이

--- Bronze/2025-11-24/2562_최댓값.js
+++ Bronze/2025-11-24/2562_최댓값.js
@@ -13,3 +13,8 @@ for (let i = 0; i < input.length; i++) {
 }
 console.log(maxValue);
 console.log(maxIndex);
+
+/*
+  -Infinity 를 한 이유는 9번째줄 첫 비교를 할 때 무조건 ture가 되기에 value[0] 이 maxValue에 담기고,
+  index 도 0 부터시작이니까 하나씩 증가하기 만약 8번째께 크다면 length만큼 하니까 index 8에서 최댓값이 담기고 index도 그 8이 나올것
+*/
